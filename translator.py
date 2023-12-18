5f5f02cf897903b651eb551006e86baec629013d
Teddiex <teddievalentine9@googlemail.com>
2023-12-18 14:44:24 +0000

    initial

---
+++ translator.py
@@ -0,0 +1,38 @@
+import discord
+from googletrans import Translator
+
+# Replace 'YOUR_DISCORD_BOT_TOKEN' with your actual Discord bot token
+TOKEN = 'MTA5MjExNjIzNzE5NjE5NzkxMA.G3Kts0.BAfIOCNX9z-HwTv5cYitOkfDU4RtPbyf54cfS0'
+
+# Replace 'YOUR_CHANNEL_ID' with the ID of the channel you want to monitor
+TARGET_CHANNEL_ID = '546084022665609222'
+
+# Initialize the bot
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# Initialize the translator
+translator = Translator()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user.name}')
+
+@client.event
+async def on_message(message):
+    # Check if the message is from the target channel
+    if str(message.channel.id) == TARGET_CHANNEL_ID:
+        # Detect the language of the message
+        source_language = translator.detect(message.content).lang
+
+        # Check if the detected language is different from English
+        if source_language != 'en':
+                # Translate the message to English
+                translation = translator.translate(message.content, dest='en')
+
+                # Send the translated text to the same channel
+                await message.channel.send(f'Translation ({source_language} to English): {translation.text}')
+
+# Run the bot
+client.run(TOKEN)
